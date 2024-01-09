05c4e7b470936a41a9ae7b65ff56d59f8f8b32f8
kiran pathade <117458423+Kiranpathade03@users.noreply.github.com>
2024-01-09 18:00:11 +0530

    first

---
+++ Inheritancetwo.java
@@ -0,0 +1,40 @@
+// 09 Jan 2024
+/**
+ * Inheritancetwo
+ */
+class Parent {
+
+  static int a = 122;
+
+  void m1() {
+    System.out.println("in m1()");
+  }
+
+  static {
+    System.out.println("In Static block Parent");
+  }
+
+  Parent() {
+    System.out.println("In Parent constctor");
+  }
+
+}
+
+public class Inheritancetwo extends Parent {
+  static {
+    System.out.println("In static child block");
+  }
+
+  Inheritancetwo() {
+    System.out.println("In Child Constuctor");
+  }
+
+  public static void main(String[] args) {
+    Inheritancetwo obj2 = new Inheritancetwo();
+    Parent.a = 12;
+    System.out.println(a);
+    System.out.println(Parent.a);
+
+  }
+
+}

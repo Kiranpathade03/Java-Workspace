5df1de1165d4492f112924a94a8cacd3d0745e62
kiran pathade <117458423+Kiranpathade03@users.noreply.github.com>
2024-02-13 14:10:59 +0530

    new

---
+++ File Handling/FileReadernew.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FileReadernew {
+    public static void main(String[] args) {
+        //read one one charcater
+        try {
+            FileReader fr = new FileReader(
+                    "C:\\Users\\kiramn patahade\\Desktop\\JAVA class\\Java-Workspace\\File Handling\\createfile.txt");
+            while (fr.ready()) {
+                System.out.println((char) fr.read());
+            }
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
+
+        // read String
+
+        try {
+            
+            BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\kiramn patahade\\Desktop\\JAVA class\\Java-Workspace\\File Handling\\createfile.txt"));
+
+            System.out.println(br.readLine());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
